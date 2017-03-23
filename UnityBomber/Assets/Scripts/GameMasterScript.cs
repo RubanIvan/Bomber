8c396eba0bd515e492215f57a0c6cb5e60b9c26a
vanurik <vanurik@mail.ru>
2017-03-23 13:39:15 +0300

    first

---
+++ UnityBomber/Assets/Scripts/GameMasterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterScript : MonoBehaviour {
+
+    public List<GameObject> HousetopNormal =new List<GameObject>();
+
+    public List<GameObject> HousetopDestr =new List<GameObject>();
+
+    public List<GameObject> House = new List<GameObject>();
+
+    public GameObject Bomb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
